62f483733efd591d952cd3f53ed139a279e87f2b
elvis <150378849+Elviiiis@users.noreply.github.com>
2025-07-09 06:41:14 -0300

    Update presos.js

--- presos.js
+++ presos.js
@@ -3,26 +3,30 @@ const tabela = document.getElementById("tabela");
 
 function atualizarTabela() {
   tabela.innerHTML = "";
+  const agora = new Date();
+  const horaAtual = `${String(agora.getHours()).padStart(2, '0')}:${String(agora.getMinutes()).padStart(2, '0')}`;
+
   cadastros.forEach((item, index) => {
     if (item.liberado) return;
 
+    const passouTempo = horaAtual >= item.horaFinal;
     const tr = document.createElement("tr");
+
     tr.innerHTML = `
       <td>${item.nome}</td>
       <td>${item.turma}</td>
       <td>${item.entrada}</td>
       <td>${item.horaFinal}</td>
       <td>${item.tipo}</td>
-      <td>
-        <button class="btn-liberar" onclick="liberar(${index})">Liberar</button>
-        <button onclick="removerCadastro(${index})">Remover</button>
-      </td>
+ <td>
+  <button onclick="liberar(${index})">Liberar</button>
+  <button onclick="removerCadastro(${index})">Remover</button>
+</td>
+
     `;
     tabela.appendChild(tr);
   });
 }
-
-
 
 function liberar(index) {
   cadastros[index].liberado = true;
